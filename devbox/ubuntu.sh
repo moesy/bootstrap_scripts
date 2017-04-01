d25d32369326edbbbbbd21db63a7ebec876c43d1
Moe Sy <moe.sy@wslogistics.io>
2017-04-01 08:32:21 +0000

    Refactor zsh & oh-my-zsh install commands
    sudo-sh option for installing oh-my-zsh will require a root password
    on AWS which does not exist.
    
    Using the curl option makes this portion of the script compatible on
    local ubuntu machines as well as ec2.

--- devbox/ubuntu.sh
+++ devbox/ubuntu.sh
@@ -1,11 +1,12 @@
 sudo apt-get update -y
 
 sudo apt-get install git
-sudo apt install build-essential checkinstall -y
 
-sudo apt-get install zsh -y
-
-# TODO: skip if already installed
-sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install build-essential -y
 
 sudo apt-get install emacs -y
+
+echo "Installing zsh & oh-my-zsh"
+sudo apt-get install zsh -y
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sudo sh
+sudo chsh -s /bin/zsh
